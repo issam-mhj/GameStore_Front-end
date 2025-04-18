efbc8311fc9842908ee1d9d88d0a219c11ce2024
wassim205 <wassimelmorabit7@gmail.com>
2025-04-18 09:49:30 +0100

    merging and fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,53 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import AuthRoute from './components/AuthRoute'; 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
-import Unauthorized from './pages/Unauthorized';
-import Layout from './components/Layout';
-import Categories from './pages/Categories';
-import Products from './pages/Products';
-import ProductGrid from './pages/ProductLists';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AuthRoute from "./components/AuthRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Unauthorized from "./pages/Unauthorized";
+import Layout from "./components/Layout";
+import Categories from "./pages/Categories";
+import Products from "./pages/Products";
+import ProductGrid from "./pages/ProductLists";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-<CartProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+        <CartProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
 
-            <Route element={<AuthRoute />}>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
+              <Route element={<AuthRoute />}>
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+              </Route>
+
+              <Route path="unauthorized" element={<Unauthorized />} />
+
+              {/* Protected routes */}
+              <Route
+                element={
+                  <ProtectedRoute roles={["product_manager", "super_admin"]} />
+                }
+              >
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="category" element={<Categories />} />
+                <Route path="manageProducts" element={<Products />} />
+              </Route>
+
+              <Route
+                element={<ProtectedRoute roles={["client", "super_admin"]} />}
+              >
+                <Route path="products" element={<ProductGrid />} />
+              </Route>
             </Route>
-
-            <Route path="unauthorized" element={<Unauthorized />} />
-
-            {/* Protected routes */}
-            <Route
-              element={
-                <ProtectedRoute roles={["product_manager", "super_admin"]} />
-              }
-            >
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="category" element={<Categories />} />
-              <Route path="manageProducts" element={<Products />} />
-            </Route>
-
-            <Route
-              element={<ProtectedRoute roles={["client", "super_admin"]} />}
-            >
-              <Route path="products" element={<ProductGrid />} />
-            </Route>
-          </Route>
-          <Route path="*" element={<div>404</div>} />
-        </Routes>
-         
+            <Route path="*" element={<div>404</div>} />
+          </Routes>
         </CartProvider>
       </AuthProvider>
     </Router>
